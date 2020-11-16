ad735f1e1d68e42efd0a76b3a20c73f754fcc118
Kelly <kelly.lan@ad-hub.net>
2020-11-16 18:27:56 +0800

    Modify config

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# laravel-inertia
+# Lightning
+
+[關於我用 Laravel 寫 SPA 卻不寫 API 的那檔事](https://ithelp.ithome.com.tw/users/20113602/ironman/3322) 系列範例，使用和 Laravel + Vue.js + Inertia.js + Tailwind CSS 構建的簡易部落格平台。
+
+## 安裝
+
+下載專案後執行 Composer：
+
+```bash
+composer install
+```
+
+設定 `.env` 檔：
+
+```bash
+cp .env.example .env
+vim .env
+
+# .env
+APP_URL=https://lightning.test
+...
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=lightning
+DB_USERNAME=[username]
+DB_PASSWORD=[password]
+```
+
+跑 Migrate：
+
+```bash
+php artisan migrate
+```
+
+然後新增本地 Storage 的公開連結：
+
+```bash
+php artisan storage:link
+```
+
+最後編譯前端資源：
+
+```bash
+yarn
+yarn dev
+```
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,27 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
+const PurgeIconsPlugin = require('purge-icons-webpack-plugin').default
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix
+  .js('resources/js/app.js', 'public/js')
+  .postCss('resources/css/app.css', 'public/css', [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('postcss-nested'),
+    require('autoprefixer')
+  ])
+  .webpackConfig({
+    output: {
+      chunkFilename: 'js/[name].js?id=[chunkhash]'
+    },
+    resolve: {
+      alias: {
+        vue$: 'vue/dist/vue.runtime.esm.js',
+        '@': path.resolve('resources/js')
+      }
+    },
+    plugins: [
+      new PurgeIconsPlugin()
+    ]
+  })
+  .sourceMaps()
+  .version()
